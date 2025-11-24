a600ef6b8808c50d9916117c70b309faa142732b
Ramon Byfield <ramon.byfield@hotmail.com>
2025-11-23 19:47:53 -0500

    progress

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+window.onload = function() {
+    const form = document.querySelector(".newsletter form");
+    const emailinp = document.getElementById("email");
+    const message = document.querySelector(".message");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const email = emailinp.value;
+
+        if (email === "") {
+            message.textContent = "Please enter a valid email address.";
+            return;
+        }
+
+        message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+
+        emailInput.value = "";
+    });
+};
